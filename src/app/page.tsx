d625a59a93ba91ba3a25efc71aaa3faf15272abd
Nigel Reoch <nigel.reoch@touchless.inc>
2025-12-03 16:34:21 -0500

    tailwind updates and shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,17 @@ import { ticketsPath } from "@/paths";
 
 const HomePage = () => {
   return (
-    <div>
-      <h2 className="text-6xl font-bold">Home Page</h2>
-      <Link href={ticketsPath()} className="underline">
-        Go to Tickets Page
-      </Link>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Home Page</h2>
+        <p className="text-sm">Your home page to start your journey</p>
+      </div>
+
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="underline">
+          Go to Tickets Page
+        </Link>
+      </div>
     </div>
   );
 };
