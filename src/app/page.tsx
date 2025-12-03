fd23fd6dc6f60190ccfd1679f56c717d3e88ffa9
Nigel Reoch <nigel.reoch@touchless.inc>
2025-12-03 16:38:02 -0500

    small change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
     <div className="flex-1 flex flex-col gap-y-8">
       <div>
         <h2 className="text-3xl font-bold tracking-tight">Home Page</h2>
-        <p className="text-sm">Your home page to start your journey</p>
+        <p className="text-sm">Your home page content goes here...</p>
       </div>
 
       <div className="flex-1 flex flex-col items-center">
